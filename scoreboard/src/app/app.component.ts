e0332119ffa082b0c64a2d9d3f444efd4fa1f97f
Batya3 <batyrannamuhammedow333gmail.com>
2024-08-22 11:36:37 +0500

    file add

---
+++ scoreboard/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {setTheme} from "ngx-bootstrap/utils";
+
+@Component({
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.less']
+})
+export class AppComponent {
+	title = 'scoreboard';
+
+	constructor() {
+		setTheme('bs3');
+	}
+}
---
+++ scoreboard/src/app/ui.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {SessionStorage} from "ngx-store";
+import {Subject} from "rxjs";
+
+/**
+ * Service storing user preferences (in session storage for persistence).
+ */
+@Injectable({
+	providedIn: 'root'
+})
+export class UiService {
+
+	@SessionStorage({key: 'showHistory'})
+	public showHistory: boolean = true;
+	@SessionStorage({key: 'showOnlySums'})
+	public showOnlySums: boolean = false;
+	@SessionStorage({key: 'showImages'})
+	public showImages: boolean = true;
+	@SessionStorage({key: 'showNotifications'})
+	public showNotifications: boolean = true;
+	@SessionStorage({key: 'darkmode'})
+	public darkmode: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	public darkmodeChanges = new Subject<boolean>();
+
+	constructor() {
+		this.setDarkmode(this.darkmode);
+	}
+
+	setDarkmode(enabled: boolean) {
+		if (enabled) {
+			document.body.parentElement.classList.add('dark');
+		} else {
+			document.body.parentElement.classList.remove('dark');
+		}
+		if (enabled != this.darkmode) {
+			this.darkmodeChanges.next(enabled);
+		}
+		this.darkmode = enabled;
+	}
+}
